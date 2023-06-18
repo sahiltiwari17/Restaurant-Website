5e7258a685ca687cbf4ddc6114e29b0165f58cc7
Sahil Tiwari <tiwarisahil116@gmail.com>
2023-06-19 04:49:59 +0530

    first commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">With each creation, we pour our hearts onto the plate,</p>
+        </div>
+        <p className="p__opensans"> Crafting flavors that dance and elevate,
+Marrying tradition with innovation, a culinary symphony,
+Indulge in our gastronomic journey, a testament to our culinary artistry.</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
